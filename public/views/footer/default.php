715e6a4fb5e4ef7f92b3b49610096e5c77cf0b4a
sbangnielsen <sbn@dagensmedicin.dk>
2019-06-05 15:56:06 +0200

    Insert biz-info between credit text and social menu

--- public/views/footer/default.php
+++ public/views/footer/default.php
@@ -1,15 +1,9 @@
-		<footer class="app-footer">
-
-<?php the_biz_info(array('4', '<div id="fotter>', '</div>')) ?>
-
-			<?php Exhale\Template\Footer::displayCredit() ?>
-
-			<?php Hybrid\View\display( 'nav/menu', 'social', [ 'location' => 'social' ] ) ?>
-
-		</footer>
-
+<footer class="app-footer">
+	<?php Exhale\Template\Footer::displayCredit() ?>
+	<?php the_biz_info(array('4', '<div id="biz-info>', '</div>')) ?>
+	<?php Hybrid\View\display( 'nav/menu', 'social', [ 'location' => 'social' ] ) ?>
+</footer>
 </div><!-- .app -->
-
 <?php wp_footer() ?>
 </body>
 </html>
