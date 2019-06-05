7cb5d1e91af4172c947b2c6f1769901e5c4f89b2
sbangnielsen <sbn@dagensmedicin.dk>
2019-06-05 14:39:01 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme functions file.
+ *
+ * This file is used to bootstrap the theme.
+ *
+ */
+
+// Uncomment the below to load the theme's `style.css` file.
+
+/* 
+add_action( 'wp_enqueue_scripts', function() {
+	wp_enqueue_style( 'exhale-unravel', get_stylesheet_uri() );
+}, 15 ); */
